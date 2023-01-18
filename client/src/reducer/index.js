3f4c3618d71b4f529b915023f99312390bc40e31
Lucas Montecino <lucasmontecino95@gmail.com>
2023-01-17 21:10:58 -0300

    Reducer Get Recipes Name

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -1,26 +1,29 @@
-
-
 const initialState = {
-    recipes: [],
-    diets: []
-}
+  recipes: [],
+  diets: [],
+};
 
-function rootReducer(state = initialState, action){
-    switch(action.type){
-        case "GET_RECIPES":
-            return {
-                ...state,
-                recipes: action.payload
-            }
-        case "GET_DIETS":
-            return {
-                ...state,
-                diets: action.payload
-            }
-        default: 
-            return state
-    }
+function rootReducer(state = initialState, action) {
+  switch (action.type) {
+    case "GET_RECIPES":
+      return {
+        ...state,
+        recipes: action.payload,
+      };
+
+    case "GET_RECIPES_NAME":
+      return {
+        ...state,
+        recipes: action.payload,
+      };
+    case "GET_DIETS":
+      return {
+        ...state,
+        diets: action.payload,
+      };
+    default:
+      return state;
+  }
 }
 
 export default rootReducer;
-
